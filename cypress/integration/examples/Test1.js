de742bc3aae80de2f2f1ba25c86e168e2af898f9
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2024-05-29 23:46:50 +0600

    Build Cypress Basic test and run from test Runner

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,1 +1,10 @@
 //cypress - Spec
+
+describe("My First Test Suit", function()
+{
+    it("My First Test Case", function()
+    {
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    })
+}
+)
