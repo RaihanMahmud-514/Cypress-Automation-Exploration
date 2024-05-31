57f7e0d5e9c76316a65c992afb04d8576fb5ce4f
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2024-05-31 11:38:50 +0600

    Visit to the URL and Partial Search

--- cypress/integration/GreenKart/Test1.js
+++ cypress/integration/GreenKart/Test1.js
@@ -1,10 +1,13 @@
 //cypress - Spec
 
+/// <reference types="Cypress"/>
+
 describe("My First Test Suite", function()
 {
     it("My First Test Case", function()
     {
-        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
     })
     
 })
