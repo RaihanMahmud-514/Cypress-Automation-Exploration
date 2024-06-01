2ada9e6fb1ab9774c0d0818011109e77096acf60
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2024-06-01 21:03:31 +0600

    Handling static dropdowns using select command

--- cypress/integration/GreenKart/Test3.js
+++ cypress/integration/GreenKart/Test3.js
@@ -7,10 +7,14 @@ describe("My Third Test Suite", function()
     it("My Third Test Case", function()
     {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
         //Select check boxes
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1')
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked')
         cy.get('input[type="checkbox"]').check(['option2', 'option3'])
+
+        //Select value from static dropdown
+        cy.get('select').select('option2').should('have.value', 'option2')
     })
     
 })
