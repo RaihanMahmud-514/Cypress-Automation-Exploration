066a9960a4fedc772c04951b3ca79e96f612e271
RaihanMahmud-514 <raihan.mahmud514@gmail.com>
2024-06-05 23:31:47 +0600

    Cypress auto handles Alerts in web Apps

---
+++ cypress/integration/GreenKart/Test4.js
@@ -0,0 +1,25 @@
+//cypress - Spec
+
+/// <reference types="Cypress"/>
+
+describe("My Third Test Suite", function()
+{
+    it("My Third Test Case", function()
+    {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get('#alertbtn').click()
+        //window:alert
+        cy.on('window:alert', (str) =>
+            {
+                expect(str).to.equal('Hello , share this practice page and share your knowledge')
+            })
+        cy.get("[value='Confirm']").click()
+
+        cy.on('window:confirm', (str) =>
+            {
+                expect(str).to.equal('Hello , Are you sure you want to confirm?')
+            })
+        
+    })
+    
+})
